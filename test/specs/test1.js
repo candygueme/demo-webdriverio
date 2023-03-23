45be1fc93b4fa82dd31adceff0596def84973bf6
candy.guerrero <Candy.X.Guerrero.-ND@disney.com>
2023-03-23 16:03:24 -0500

    adding suites

--- test/specs/test1.js
+++ test/specs/test1.js
@@ -19,7 +19,7 @@ describe('my awesome website', () => {
     });
 
     it('should do some assertions', async () => {
-        await expect(browser).toHaveUrl('https://webdriver.io');
+        await expect(browser).toHaveUrl('https://webdriver.io/');
         await expect(browser).toHaveUrlContaining('webdriver');
         await expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
         await expect(browser).toHaveTitleContaining('WebdriverIO');
@@ -33,21 +33,28 @@ describe('my awesome website', () => {
         await expect(elem).toExist();
         await expect(elem).toBePresent();
         await expect(elem).toBeClickable();
-        await expect(elem).toBeDisabled();
     })
 
 
     it('expect the same title with chai', async () => {
+        const elem = $("//a[text()='Why WebdriverIO?']")
+        await elem.waitForClickable();
         expectChai(await browser.getTitle()).to.equal('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
+        expectChai(await elem.isDisplayed()).to.be.true;
+        const elemt2= $('.hero__subtitle')
+        expectChai(await elemt2.getText()).to.include('Next-gen browser')
+        const array = []
+        expectChai(array).to.be.empty;
+        expectChai(array).to.be.an('array').that.is.empty;
     })
 
     it('test case 3', async () => {
         // ...
     });
 
-    it.only('test case 3', async () => {
+   // it.only('test case 3', async () => {
         // ...
-    });
+   // });
 
     it.skip('test case 4', async () => {
         // ...
